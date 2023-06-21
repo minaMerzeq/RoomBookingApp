b89ec1ce78f243f10bc53e0abc9873de73415ca4
minaMerzeq <minamerzekluffy@gmail.com>
2023-06-21 19:40:45 +0300

    write code to pass test

--- RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -8,13 +8,17 @@ namespace RoomBookingApp.Core.Processors
         {
         }
 
-        public RoomBookingResponse BookRoom(RoomBookingRequest request) =>
-            new RoomBookingResponse
+        public RoomBookingResponse BookRoom(RoomBookingRequest request)
+        {
+            if(request is null)
+                throw new ArgumentNullException(nameof(request));
+
+            return new()
             {
                 FullName = request.FullName,
                 Email = request.Email,
                 Date = request.Date,
             };
-
+        }
     }
 }
